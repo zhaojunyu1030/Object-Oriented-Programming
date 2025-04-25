5fd111ea9d770d6215308bfbbf151e705cd634c2
junyu <jz6096@nyu.edu>
2025-04-25 19:41:54 -0400

    Create rec09_assigning.cpp

---
+++ Lab/rec09/rec09_assigning.cpp
@@ -0,0 +1,38 @@
+/* rec09_assigning.cpp
+ * Created by Junyu(Leo) Zhao on 3/21/2025
+ */
+
+#include <iostream>
+
+using namespace std;
+class Base {
+public:
+    Base(int num) : num(num) { }
+private:
+    int num;
+};
+
+
+class Derived : public Base {
+public:
+    Derived(int num) : Base(num) { }
+};
+
+int main() {
+    Base base(0);
+    Derived derived(1);
+
+    Base* bptr = nullptr;
+    Derived* dptr = nullptr;
+
+    // derived = base;  // Derived-specific data may be lost
+    base = derived;
+
+    // dptr = bptr;
+    bptr = dptr;
+
+    bptr = &base;
+    dptr = &derived;
+    bptr = &derived;
+    // dptr = &base;
+}
